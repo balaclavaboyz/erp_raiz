9638a2b2ce1fa170ae1a19880edf74f0169b6a60
peter <peterthomaskim@gmail.com>
2024-02-02 14:01:28 -0300

    + from nota entrada, inserted new prods rows with global vars edit schema table prods
    init in get pl to show group by month with pandas

--- nota_entrada.py
+++ nota_entrada.py
@@ -1,13 +1,19 @@
 from glob import glob
+
+import pandas
 import xmltodict
 from pprint import pp
 from collections import deque
 import sqlite3
 
+SIMPLES_NACIONAL = 0.04
+ML_COMISS = 0.12
+
 
 class NotaEntrada:
     def __init__(self):
         def create_db():
+
             # nota de entrada
             self.cur.execute('''create table if not exists entrada 
             (id integer primary key autoincrement,
@@ -28,11 +34,8 @@ class NotaEntrada:
             nome text,
             quant int,
             valor_uni real,
-            valor_bc_icms real,
-            valor_icms real,
             valor_icms_devido real,
-            valor_aliquota_icms real,
-            ecan int,
+            cean int,
             ml_comiss real,
             ml_taxa real,
             foreign key(fk_chave_entrada) references entrada(chave),
@@ -104,6 +107,7 @@ class NotaEntrada:
                 # self.prods.append(x['nfeProc']['NFe']['infNFe']['det'])
                 # self.nome.append(x['nfeProc']['NFe']['infNFe']['emit']['CNPJ'])
                 # self.icms.append(x['nfeProc']['NFe']['infNFe']['total']['ICMSTot'])
+
                 cnpj = x['nfeProc']['NFe']['infNFe']['emit']['CNPJ']
                 chave = x['nfeProc']['NFe']['infNFe']['@Id']
                 emissao = x['nfeProc']['NFe']['infNFe']['ide']['dhEmi']
@@ -112,11 +116,77 @@ class NotaEntrada:
                 valor_icms = float(x['nfeProc']['NFe']['infNFe']['total']['ICMSTot']['vICMS'])
                 valor_icms_devido = float(valor_bc * (0.18 - (valor_icms / valor_bc)))
                 valor_frete = float(x['nfeProc']['NFe']['infNFe']['total']['ICMSTot']['vFrete'])
+
                 self.cur.execute('''insert into entrada
                 (cnpj,chave,emissao,valor_total,valor_bc,valor_icms,valor_icms_devido,valor_frete) values
                 (?,?,?,?,?,?,?,?)''', (
                     cnpj, chave, emissao, valor_total, valor_bc, valor_icms, valor_icms_devido, valor_frete))
                 self.con.commit()
+
+                prods = x['nfeProc']['NFe']['infNFe']['det']
+
+                def parse_prods(one_prod: dict):
+                    q_cean = one_prod['prod']['cEAN']
+                    q_xProd = one_prod['prod']['xProd']
+                    q_qCom = one_prod['prod']['qCom']
+                    try:
+                        q_frete = float(one_prod['prod']['vFrete'])
+                    except KeyError:
+                        q_frete = 0.0
+                    q_valor_uni = float(one_prod['prod']['vUnCom'])
+
+                    # TODO see if decimal notation is used
+                    q_icms_bc = float(one_prod['imposto']['ICMS']['ICMS00']['vBC'])
+                    q_icms_porcent = float(one_prod['imposto']['ICMS']['ICMS00']['pICMS']) / 100
+                    q_icms_devido = q_icms_bc * (0.18 - q_icms_porcent)
+                    # taxa_fixa, can be only be calc after sell not before
+                    q_taxa_fixa = 6 if (q_valor_uni + q_frete + 6) / (
+                            1 - SIMPLES_NACIONAL - ML_COMISS) >= 79 else 19  # another convoluted math to calculate if the taxa fixa is 6 or 19
+                    return q_cean, q_xProd, q_qCom, q_frete, q_valor_uni, q_icms_devido, q_taxa_fixa
+
+                def db_insert_prod(i_chave, i_xProd, i_qCom, i_valor_uni, i_icms_devido, i_cean, i_taxa_fixa):
+                    self.cur.execute('''insert into prods
+                    (fk_chave_saida,
+                    nome,
+                    quant,
+                    valor_uni,
+                    valor_icms_devido,
+                    cean,
+                    ml_comiss,
+                    ml_taxa)values(?,?,?,?,?,?,?,?)''', (
+                        i_chave,  # fk_chave_saida,
+                        i_xProd,  # nome,
+                        i_qCom,  # quant,
+                        i_valor_uni,  # valor_uni,
+                        i_icms_devido,  # valor_icms_devivo,
+                        i_cean,  # cean,
+                        ML_COMISS,
+                        # ml_comiss, TODO placeholder default .12, future added a manual method to find the real value online and replace it
+                        i_taxa_fixa
+                        # ml_taxa TODO same above, but can only calculate the real one if the above is correct
+                    ))
+                    self.con.commit()
+
+                if type(prods) is list:
+                    for prod in prods:
+                        cean, xProd, qCom, frete, valor_uni, icms_devido, taxa_fixa = parse_prods(prod)
+
+                        db_insert_prod(i_chave=chave,
+                                       i_xProd=xProd,
+                                       i_qCom=qCom,
+                                       i_valor_uni=valor_uni,
+                                       i_icms_devido=icms_devido,
+                                       i_cean=cean,
+                                       i_taxa_fixa=taxa_fixa)
+                else:
+                    cean, xProd, qCom, frete, valor_uni, icms_devido, taxa_fixa = parse_prods(prods)
+                    db_insert_prod(i_chave=chave,
+                                   i_xProd=xProd,
+                                   i_qCom=qCom,
+                                   i_valor_uni=valor_uni,
+                                   i_icms_devido=icms_devido,
+                                   i_cean=cean,
+                                   i_taxa_fixa=taxa_fixa)
 
     def receber_saida(self):
         all_saidas = glob('./saida/*.xml')
@@ -181,10 +251,11 @@ class NotaEntrada:
         icms_devido = 0.0
         valor_venda = 0.0
         valor_produto = 0.0
-        self.cur.execute('select valor_total from entrada')
+        emissao_entrada = []
+        self.cur.execute('select valor_total,emissao from entrada')
         for i in self.cur.fetchall():
             valor_produto += float(i[0])
-
+            emissao_entrada.append(i[1])
         self.cur.execute('select valor_icms_devido from entrada')
         for i in self.cur.fetchall():
             icms_devido += float(i[0])
@@ -194,6 +265,9 @@ class NotaEntrada:
         for i in self.cur.fetchall():
             valor_venda += float(i[0])
 
+        df = pandas.to_datetime(pandas.Series(emissao_entrada))
+        pp(df.dt.to_period('M'))
+        # TODO group by month
         pp(icms_devido)
         pp(valor_venda)
         pp(valor_produto)
