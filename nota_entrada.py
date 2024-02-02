9146525646d3ed236e33a167ecfd908c652b6a40
peter <peterthomaskim@gmail.com>
2024-02-02 15:23:20 -0300

    changed valor unitario to valor total in table prods, bc wont need individual value and added date time
    finished nota entrada and saida

--- nota_entrada.py
+++ nota_entrada.py
@@ -33,11 +33,12 @@ class NotaEntrada:
             fk_chave_saida integer,
             nome text,
             quant int,
-            valor_uni real,
+            valor_tot real,
             valor_icms_devido real,
             cean int,
             ml_comiss real,
             ml_taxa real,
+            emissao text,
             foreign key(fk_chave_entrada) references entrada(chave),
             foreign key(fk_chave_saida) references saida(chave))''')
 
@@ -133,58 +134,53 @@ class NotaEntrada:
                         q_frete = float(one_prod['prod']['vFrete'])
                     except KeyError:
                         q_frete = 0.0
-                    q_valor_uni = float(one_prod['prod']['vUnCom'])
+                    q_valor_tot = float(one_prod['prod']['vProd'])*-1
 
                     # TODO see if decimal notation is used
                     q_icms_bc = float(one_prod['imposto']['ICMS']['ICMS00']['vBC'])
                     q_icms_porcent = float(one_prod['imposto']['ICMS']['ICMS00']['pICMS']) / 100
                     q_icms_devido = q_icms_bc * (0.18 - q_icms_porcent)
                     # taxa_fixa, can be only be calc after sell not before
-                    q_taxa_fixa = 6 if (q_valor_uni + q_frete + 6) / (
+                    q_taxa_fixa = 6 if (q_valor_tot + q_frete + 6) / (
                             1 - SIMPLES_NACIONAL - ML_COMISS) >= 79 else 19
                     # another convoluted math to calculate if the taxa fixa is 6 or 19
-                    return q_cean, q_xProd, q_qCom, q_frete, q_valor_uni, q_icms_devido, q_taxa_fixa
+                    return q_cean, q_xProd, q_qCom, q_frete, q_valor_tot, q_icms_devido, q_taxa_fixa
 
-                def db_insert_prod(i_chave, i_xProd, i_qCom, i_valor_uni, i_icms_devido, i_cean, i_taxa_fixa):
+                def db_insert_prod(i_chave, i_xProd, i_qCom, i_valor_tot, i_icms_devido, i_cean, i_taxa_fixa):
                     self.cur.execute('''insert into prods
-                    (fk_chave_saida,
+                    (fk_chave_entrada,
                     nome,
                     quant,
-                    valor_uni,
+                    valor_tot,
                     valor_icms_devido,
-                    cean,
-                    ml_comiss,
-                    ml_taxa)values(?,?,?,?,?,?,?,?)''', (
+                    cean
+                    )values(?,?,?,?,?,?)''', (
                         i_chave,  # fk_chave_saida,
                         i_xProd,  # nome,
                         i_qCom,  # quant,
-                        i_valor_uni,  # valor_uni,
+                        i_valor_tot,  # valor_uni,
                         i_icms_devido,  # valor_icms_devivo,
-                        i_cean,  # cean,
-                        ML_COMISS,
-                        # ml_comiss, TODO placeholder default .12, future added a manual method to find the real value online and replace it
-                        i_taxa_fixa
-                        # ml_taxa TODO same above, but can only calculate the real one if the above is correct
+                        i_cean  # cean,
                     ))
                     self.con.commit()
 
                 if type(prods) is list:
                     for prod in prods:
-                        cean, xProd, qCom, frete, valor_uni, icms_devido, taxa_fixa = parse_prods(prod)
+                        cean, xProd, qCom, frete, valor_tot, icms_devido, taxa_fixa = parse_prods(prod)
 
                         db_insert_prod(i_chave=chave,
                                        i_xProd=xProd,
                                        i_qCom=qCom,
-                                       i_valor_uni=valor_uni,
+                                       i_valor_tot=valor_tot,
                                        i_icms_devido=icms_devido,
                                        i_cean=cean,
                                        i_taxa_fixa=taxa_fixa)
                 else:
-                    cean, xProd, qCom, frete, valor_uni, icms_devido, taxa_fixa = parse_prods(prods)
+                    cean, xProd, qCom, frete, valor_tot, icms_devido, taxa_fixa = parse_prods(prods)
                     db_insert_prod(i_chave=chave,
                                    i_xProd=xProd,
                                    i_qCom=qCom,
-                                   i_valor_uni=valor_uni,
+                                   i_valor_tot=valor_tot,
                                    i_icms_devido=icms_devido,
                                    i_cean=cean,
                                    i_taxa_fixa=taxa_fixa)
@@ -195,58 +191,89 @@ class NotaEntrada:
             with open(i, encoding='utf-8') as f:
                 x = xmltodict.parse(f.read())
 
+                # static parms for inserting into saida table
+                emissao = x['nfeProc']['NFe']['infNFe']['ide']['dhEmi']
+                frete = x['nfeProc']['NFe']['infNFe']['total']['ICMSTot']['vFrete']
+                chave = x['nfeProc']['NFe']['infNFe']['@Id']
+                try:
+                    cpf_cnpj = x['nfeProc']['NFe']['infNFe']['dest']['CPF']
+                except KeyError:
+                    cpf_cnpj = x['nfeProc']['NFe']['infNFe']['dest']['CNPJ']
+                valor_total = x['nfeProc']['NFe']['infNFe']['total']['ICMSTot']['vProd']
+
+                self.cur.execute('''insert into saida
+                (emissao,
+                valor_total,
+                frete,
+                chave,
+                cpf_cnpj)values
+                (?,?,?,?,?)''', (
+                    emissao,
+                    valor_total,
+                    frete,
+                    chave,
+                    cpf_cnpj
+                ))
+
                 if type(x['nfeProc']['NFe']['infNFe']['det']) is list:
                     for one_prod in x['nfeProc']['NFe']['infNFe']['det']:
                         emissao = x['nfeProc']['NFe']['infNFe']['ide']['dhEmi']
-                        # cean = one_prod['prod']['cEAN']
-                        # quant = one_prod['prod']['qCom']
-                        valor_total = one_prod['prod']['vProd']
-                        frete = x['nfeProc']['NFe']['infNFe']['total']['ICMSTot']['vFrete']
+                        cean = one_prod['prod']['cEAN']
+                        quant = one_prod['prod']['qCom']
+                        valor_tot = one_prod['prod']['vProd']
                         chave = x['nfeProc']['NFe']['infNFe']['@Id']
+                        nome = one_prod['prod']['xProd']
+                        ml_taxa = 6 if float(valor_tot) <= 79 else 20
 
-                        try:
-                            cpf_cnpj = x['nfeProc']['NFe']['infNFe']['dest']['CPF']
-                        except KeyError as e:
-                            cpf_cnpj = x['nfeProc']['NFe']['infNFe']['dest']['CNPJ']
-
-                        # cep = x['nfeProc']['NFe']['infNFe']['dest']['enderDest']['CEP']
-                        # rua = x['nfeProc']['NFe']['infNFe']['dest']['enderDest']['xLgr']
-                        # municipio = x['nfeProc']['NFe']['infNFe']['dest']['enderDest']['xMun']
-                        # uf = x['nfeProc']['NFe']['infNFe']['dest']['enderDest']['UF']
-                        # pais = x['nfeProc']['NFe']['infNFe']['dest']['enderDest']['xPais']
-                        # nome = x['nfeProc']['NFe']['infNFe']['dest']['xNome']
-                        # complemento = x['nfeProc']['NFe']['infNFe']['dest']['enderDest']['xCpl']
-                        self.cur.execute('''insert into saida
-                        (emissao,valor_total,frete,chave,cpf_cnpj) values(?,?,?,?,?)''',
-                                         (emissao, valor_total, frete, chave, cpf_cnpj,))
+                        self.cur.execute('''insert into prods
+                        (fk_chave_saida,
+                         nome,
+                         quant,
+                         valor_tot,
+                         cean,
+                         ml_comiss,
+                         ml_taxa,
+                         emissao) values(?,?,?,?,?,?,?,?)''', (
+                            chave,
+                            nome,
+                            quant,
+                            valor_tot,
+                            cean,
+                            ML_COMISS,
+                            ml_taxa,
+                            emissao
+                        ))
                         self.con.commit()
                 else:
                     # fixo
                     one_prod = x['nfeProc']['NFe']['infNFe']['det']
 
-                    # eh a msm function acima
                     emissao = x['nfeProc']['NFe']['infNFe']['ide']['dhEmi']
-                    # cean = one_prod['prod']['cEAN']
-                    # quant = one_prod['prod']['qCom']
-                    valor_total = one_prod['prod']['vProd']
-                    frete = x['nfeProc']['NFe']['infNFe']['total']['ICMSTot']['vFrete']
+                    cean = one_prod['prod']['cEAN']
+                    quant = one_prod['prod']['qCom']
+                    valor_tot = one_prod['prod']['vProd']
                     chave = x['nfeProc']['NFe']['infNFe']['@Id']
+                    nome = one_prod['prod']['xProd']
+                    ml_taxa = 6 if float(valor_tot) <= 79 else 20
 
-                    try:
-                        cpf_cnpj = x['nfeProc']['NFe']['infNFe']['dest']['CPF']
-                    except KeyError as e:
-                        cpf_cnpj = x['nfeProc']['NFe']['infNFe']['dest']['CNPJ']
-
-                    # cep = x['nfeProc']['NFe']['infNFe']['dest']['enderDest']['CEP']
-                    # rua = x['nfeProc']['NFe']['infNFe']['dest']['enderDest']['xLgr']
-                    # municipio = x['nfeProc']['NFe']['infNFe']['dest']['enderDest']['xMun']
-                    # uf = x['nfeProc']['NFe']['infNFe']['dest']['enderDest']['UF']
-                    # pais = x['nfeProc']['NFe']['infNFe']['dest']['enderDest']['xPais']
-                    # nome = x['nfeProc']['NFe']['infNFe']['dest']['xNome']
-                    # complemento = x['nfeProc']['NFe']['infNFe']['dest']['enderDest']['xCpl']
-                    self.cur.execute('''insert into saida
-                        (emissao,valor_total,frete,chave,cpf_cnpj) values(?,?,?,?,?)''',
-                                     (emissao, valor_total, frete, chave, cpf_cnpj,))
+                    self.cur.execute('''insert into prods
+                        (fk_chave_saida,
+                         nome,
+                         quant,
+                         valor_tot,
+                         cean,
+                         ml_comiss,
+                         ml_taxa,
+                         emissao) values(?,?,?,?,?,?,?,?)''', (
+                        chave,
+                        nome,
+                        quant,
+                        valor_tot,
+                        cean,
+                        ML_COMISS,
+                        ml_taxa,
+                        emissao
+                    ))
                     self.con.commit()
 
     def get_pl(self) -> float:
@@ -268,16 +295,16 @@ class NotaEntrada:
         # for i in self.cur.fetchall():
         #     valor_venda += float(i[0])
 
-        dfe = pandas.read_sql_query('select valor_total, emissao, valor_icms_devido, valor_frete from entrada', self.con)
+        dfe = pandas.read_sql_query('select valor_total, emissao, valor_icms_devido, valor_frete from entrada',
+                                    self.con)
         dfe['emissao'] = pandas.to_datetime(dfe['emissao'])
-        dfe.set_index('emissao',inplace=True)
+        dfe.set_index('emissao', inplace=True)
         pp(dfe.groupby(pandas.Grouper(freq='ME')).sum())
 
-        df = pandas.read_sql_query('select valor_total, emissao from saida',self.con)
+        df = pandas.read_sql_query('select valor_total, emissao from saida', self.con)
         df['emissao'] = pandas.to_datetime(df['emissao'])
         df.set_index('emissao', inplace=True)
         pp(df.groupby(pandas.Grouper(freq='ME')).sum())
-
 
         # return -icms_devido + valor_venda - (0.04 * valor_venda) - valor_produto
         return 0.0
